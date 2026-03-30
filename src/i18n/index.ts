4fac33971ce49aecd22f57125eb29110c9588c22
govorec <govorec@gmail.com>
2026-03-30 12:35:26 +0300

    feat: localization to French, Spanish, Ukrainian, Russian, Simplified Chinese and Japanese languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,10 +2,22 @@ import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
 import en from './en.json';
 import de from './de.json';
+import fr from './fr.json';
+import es from './es.json';
+import uk from './uk.json';
+import ru from './ru.json';
+import zh from './zh.json';
+import ja from './ja.json';
 
 const translations = {
   en,
   de,
+  fr,
+  es,
+  uk,
+  ru,
+  zh,
+  ja,
 };
 
 const i18n = new I18n(translations);
